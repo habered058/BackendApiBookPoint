fc663636e5e45f36c4efaf6f3a85ca67d0922aee
Harry Bernate <56971400+habered058@users.noreply.github.com>
2024-10-09 19:49:13 -0500

    Feature_001_auth | Servicios básicos e autenticación e instalación de JWT

---
+++ routes/auth.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/login', function () {
+    return 'welcome';
+});
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
